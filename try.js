efde89792be2b6d2013f2fb19907df6b2e9ba2fb
Yulia Balakis <yuliabalakis@gmail.com>
2022-07-10 19:19:27 +0300

    modified try.js

--- try.js
+++ try.js
@@ -1,11 +1,17 @@
-/* In a try catch construction, wrap the code: console.log (a), let a = 3. And display an error - ‘let must be declared’ 
-before use. When running 1/0, the error 'cannot be divided by zero' */
+/* 1. In a try catch construction, wrap the code: console.log (a), let a = 3. 
+And display an error - ‘let must be declared’ before use. */
 try{
     console.log(a);
     let a = 3;   
    } catch(err){
     console.log("let must be declare");
-    if (1/0) {
-        console.log("cannot be divided by zero");
-      }
-};
+  };
+
+/* 2. When running 1/0, the error 'cannot be divided by zero' */
+try{
+  if(1/0){
+    throw new Error("cannot be divided by zero");
+  }
+ } catch(err){
+    console.log(err.message);
+ };
